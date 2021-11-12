eb193bd3a96d59bbe9e28d5292531468bf9eba17
skysaw144 <alexandermesko@icloud.com>
2021-11-12 14:15:21 -0500

    all-cpu flag

--- research-code/run-perf.sh
+++ research-code/run-perf.sh
@@ -2,5 +2,5 @@
 pid=`ps ax | grep "type=renderer" | awk '{ print $1; }' | head -1`
 
 # --all-user is not valid; 
-perf stat -I 200 --all-user -e instructions,cycles,cache-references,cache-misses,branch-misses,bus-cycles -p $pid -o perf.out
+perf stat -I 200 --all-cpu -e instructions,cycles,cache-references,cache-misses,branch-misses,bus-cycles -p $pid -o perf.out
 
